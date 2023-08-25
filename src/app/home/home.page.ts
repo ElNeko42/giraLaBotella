980999d09e1d341bfb2be9aa1679065f7fdd1f26
ElNeko42 <franson177@gmail.com>
2023-08-25 15:59:13 +0200

    retos texto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,7 +9,26 @@ export class HomePage {
 
   constructor() { }
 
-  challenges: string[] = ['Reto 1', 'Reto 2', 'Reto 3', 'Reto 4', 'Reto 5','Reto 6', 'Reto 7', 'Reto 8', 'Reto 9', 'Reto 10','Reto 11', 'Reto 12', 'Reto 13', 'Reto 14', 'Reto 15']; // Puedes añadir más retos
+  challenges: string[] = ['Castigo: Bebe un chupito.',
+    'Confesión picante: Cuéntanos tu fantasía más oscura.',
+    'Beso a aleatorio: Gira la botella, besa ala persona que señale la botella.',
+    'Baile atrevido: Haz un baile sensual por 30 segundos.',
+    'Verdad a quemarropa: Contesta con honestidad la pregunta que te haga la persona a tu derecha.',
+    'Llamada traviesa: Llama a alguien y háblale con voz seductora durante 1 minuto.',
+    'Postura del kamasutra: Elige una postura al azar y simula cómo se haría (sin desnudarse ni contacto).',
+    'Masaje de 1 minuto: Da un masaje en los hombros o el cuello a la persona a tu izquierda.',
+    'Reparte castigo: reparte 3 chupitos a 3 personas distintas',
+    'Todos castigados: todos beben un chupito',
+    'Reto del hielo: Pasa un cubo de hielo por el cuello ala persona de la derecha hasta que se derrita.',
+    'Castigo doble: Bebe dos chupito.',
+    'Imitación: Imita alguien de la mesa',
+    'chupito sin manos: Intenta tomar un chupito sin usar tus manos no te pueden ayudar.',
+    'Duelo de miradas: Mira fijamente a alguien mientras ambos toman un chupito. El primero en reírse o desviar la mirada, toma otro.',
+    'Castigo triple: Bebe tres chupito.',
+    'Duelo de chupitos: Desafía a alguien a ver quién puede tomar un chupito más rápido.',
+    'La estatua humana: Cuando te toque, conviértete en una "estatua" no te puedes mover ni hablar durante 1 minuto',
+    'Inventa regla: inventate una regla si alguien no la cumple tiene un castigo que decide el grupo'
+  ]; 
   lastChallenge: string = '';
 
   spinBottle() {
